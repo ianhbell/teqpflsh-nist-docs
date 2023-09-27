ff9fd093f706d62e13be3fc64e0d3e6f62643507
Ian Bell <ian.bell@nist.gov>
2023-09-27 16:25:05 -0400

    Move everything into a single python script

--- prepare_build.py
+++ prepare_build.py
@@ -4,11 +4,26 @@ populated with the necessary conda and pip packages
 and a conda environment is loaded
 """
 
-import subprocess, os, sys
+import subprocess, os, sys, shutil
 here = os.path.dirname(__file__)
 
 # pip install teqp
-subprocess.check_call([sys.executable, '-m','pip','install teqp'], shell=True)
+# subprocess.check_call([sys.executable, '-m','pip','install teqp'], shell=True)
 
 # or build from source ???
 print('Finished prepare_build.py')
+
+os.makedirs("externals/teqp/doc/source/_static", exist_ok=True)
+os.makedirs("externals/teqp/doc-BORGED/source/_static", exist_ok=True)
+shutil.copy2("externals/teqp/doc-BORGED/macros.tex", "externals/teqp/doc")
+
+os.chdir('externals/teqp/doc-BORGED/source')
+
+import sys
+sys.path.append(os.path.abspath(os.curdir))
+
+import sphinx_pre_run
+sphinx_pre_run.run()
+
+os.chdir(os.path.dirname(__file__))
+shutil.move("externals/teqp/doc/source/_static/doxygen", "externals/teqp/doc-BORGED/source/_static/doxygen")
