0d05ed665336f5a37fec3948da1b275bc349bf55
Ian Bell <ian.bell@nist.gov>
2023-09-29 11:47:08 -0400

    Move from pre-build to pre-borg

--- prepare_build.py
+++ prepare_build.py
@@ -17,19 +17,19 @@ here = os.path.dirname(__file__)
 # print('Env updating complete')
 # subprocess.check_call('conda list', shell=True)
 
-# Copy back from doc-BORGED into doc some files that need to be in specific places
-os.makedirs("externals/teqp/doc/source/_static", exist_ok=True)
-os.makedirs("externals/teqp/doc-BORGED/source/_static", exist_ok=True)
-shutil.copy2("externals/teqp/doc-BORGED/macros.tex", "externals/teqp/doc")
+# # Copy back from doc-BORGED into doc some files that need to be in specific places
+# os.makedirs("externals/teqp/doc/source/_static", exist_ok=True)
+# os.makedirs("externals/teqp/doc-BORGED/source/_static", exist_ok=True)
+# shutil.copy2("externals/teqp/doc-BORGED/macros.tex", "externals/teqp/doc")
 
 # And now run doxygen and execute jupyter notebooks
-os.chdir('externals/teqp/doc-BORGED/source')
+os.chdir('externals/teqp/doc/source')
 sys.path.append(os.path.abspath(os.curdir))
 import sphinx_pre_run
 sphinx_pre_run.run()
 
-# And move the doxygen docs back where they are needed in the doc-BORGED tree
-os.chdir(os.path.dirname(__file__))
-shutil.move("externals/teqp/doc/source/_static/doxygen", "externals/teqp/doc-BORGED/source/_static/doxygen")
+# # And move the doxygen docs back where they are needed in the doc-BORGED tree
+# os.chdir(os.path.dirname(__file__))
+# shutil.move("externals/teqp/doc/source/_static/doxygen", "externals/teqp/doc-BORGED/source/_static/doxygen")
 
 print('Finished prepare_build.py')
